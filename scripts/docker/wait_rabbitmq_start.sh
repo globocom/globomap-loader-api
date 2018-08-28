97531b98c1b12a322b8f02b1282ac315a53ec2dd
Ederson Brilhante <contato@edersonbrilhante.com.br>
2018-08-28 19:33:58 -0300

    Change docker-compose to fix delay in creating keystone container API wasn't starting

---
+++ scripts/docker/wait_rabbitmq_start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Check rabbitmq service
+INTERVAL=5
+MAX_ATTEMPTS=30
+CHECK_COMMAND="curl -sL -w "%{http_code}" http://globomap_loader_queue:15672 -o /dev/null"
+ATTEMPTS=1
+HTTP_RET_CODE=`${CHECK_COMMAND}`
+while [ "000" = "${HTTP_RET_CODE}" ] && [ ${ATTEMPTS} -le ${MAX_ATTEMPTS} ]; do
+    echo "Error connecting to rabbitmq. Attempt ${ATTEMPTS}. Retrying in ${INTERVAL} seconds ..."
+    sleep ${INTERVAL}
+    ATTEMPTS=$((ATTEMPTS+1))
+    HTTP_RET_CODE=`${CHECK_COMMAND}`
+done
+
+if [ "000" = "${HTTP_RET_CODE}" ]; then
+        echo "Error connecting to rabbitmq. Aborting ..."
+        exit 2
+fi
+
+PYTHONPATH=`pwd`:$PYTHONPATH python3.6 globomap_loader_api/run.py
